6f101e0cb8748e88f52023be02742ca726ba569e
piazzatron <michael.piazza.mp@gmail.com>
2019-11-22 21:00:32 -0800

    seqlize setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import express from 'express'
+import Sequelize from 'sequelize'
 
 const app = express()
+const sequelize = new Sequelize('camera', 'postgres', 'postgres', {
+    host: 'localhost',
+    port: 5433,
+    dialect: 'postgres'
+})
+
+sequelize.authenticate().then(() => {
+    console.log("Connection established successfully")
+}).catch((err) => {
+    console.log(`Got error ${err}`)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello world')
@@ -9,3 +21,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log("I'm listening on port 3000 lol")
 })
+
